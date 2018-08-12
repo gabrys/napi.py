791a1b50e877c6187356324d0e7d4da7189f8e6e
emkor93 <emkor93@gmail.com>
2018-08-12 17:55:00 +0200

    ported script to python 3, corrected tab/spaces and formatting to match PEP rules

--- napiprojekt.py
+++ napiprojekt.py
@@ -1,44 +1,48 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
 #
 # Script downloads subtitles from napiprojekt
 #
 # based on older script
 # by gim,krzynio,dosiu,hash 2oo8.
-# last modified: 6-I-2oo8
+# last modified: 2018-08-12
 # 4pc0h f0rc3
 
-import md5, sys, urllib, os
+import sys
+from urllib import request
+import os
+import hashlib
 
 from tempfile import NamedTemporaryFile
 from subprocess import Popen, PIPE
 
 
 def f(z):
-	idx = [ 0xe, 0x3,  0x6, 0x8, 0x2 ]
-	mul = [   2,   2,    5,   4,   3 ]
-	add = [   0, 0xd, 0x10, 0xb, 0x5 ]
+    idx = [0xe, 0x3, 0x6, 0x8, 0x2]
+    mul = [2, 2, 5, 4, 3]
+    add = [0, 0xd, 0x10, 0xb, 0x5]
 
-	b = []
-	for i in xrange(len(idx)):
-		a = add[i]
-		m = mul[i]
-		i = idx[i]
+    b = []
+    for i in range(len(idx)):
+        a = add[i]
+        m = mul[i]
+        i = idx[i]
 
-		t = a + int(z[i], 16)
-		v = int(z[t:t+2], 16)
-		b.append( ("%x" % (v*m))[-1] )
+        t = a + int(z[i], 16)
+        v = int(z[t:t + 2], 16)
+        b.append(("%x" % (v * m))[-1])
 
-	return "".join(b)
+    return "".join(b)
 
 
 def napiurl(path):
-    d = md5.new();
-    d.update(open(path).read(10485760))
+    d = hashlib.md5()
+    d.update(open(path, mode='rb').read(10485760))
     h = d.hexdigest()
 
     return "http://napiprojekt.pl/unit_napisy/dl.php?l=PL&" + \
-        "f=" + h + "&t=" + f(h) + \
-        "&v=other&kolejka=false&nick=&pass=&napios=" + os.name
+           "f=" + h + "&t=" + f(h) + \
+           "&v=other&kolejka=false&nick=&pass=&napios=" + os.name
 
 
 class Un7ZipError(Exception):
@@ -56,13 +60,13 @@ def un7zip(archive, password=None, tmpfileprefix="un7zip", tmpfilesuffix=".7z"):
     cmd += [tmpfile.name]
 
     sp = Popen(cmd, stdout=PIPE, stderr=PIPE)
-        
+
     content = sp.communicate()[0]
 
     if sp.wait() != 0:
         raise Un7ZipError("Invalid archive")
 
-    tmpfile.close() # deletes the file
+    tmpfile.close()  # deletes the file
     return content
 
 
@@ -77,7 +81,7 @@ class NoMatchingSubtitle(Exception):
 
 def download_subtitle(filename):
     url = napiurl(filename)
-    content_7z = urllib.urlopen(url).read()
+    content_7z = request.urlopen(url).read()
     try:
         content = un7zip(content_7z, password="iBlm8NTigvru0Jr0")
     except Un7ZipError:
@@ -85,17 +89,17 @@ def download_subtitle(filename):
 
     # Don't override the same subtitles
     try:
-        same = open(subtitlepath(filename), "r").read() == content
+        same = open(subtitlepath(filename), "rb").read() == content
     except IOError:
         same = False
 
     if not same:
-        open(subtitlepath(filename), "w").write(content)
+        open(subtitlepath(filename), "wb").write(content)
 
 
 def main():
     if len(sys.argv) < 2:
-        sys.stderr.write("\nUSAGE:\n\t" + sys.argv[0] + " moviefile [moviefile, ...]\n\n")
+        print("\nUSAGE:\n\t" + sys.argv[0] + " moviefile [moviefile, ...]\n\n")
         exit(1)
 
     failed = False
@@ -103,15 +107,15 @@ def main():
         for arg in sys.argv[1:]:
             try:
                 download_subtitle(arg)
-                print "OK " + arg
+                print("OK " + arg)
             except NoMatchingSubtitle:
                 failed = True
-                print "NOSUBS " + arg
+                print("NOSUBS " + arg)
             except IOError:
-                sys.stderr.write("Cannot read file " + arg + "\n")
+                print("Cannot read file " + arg + "\n")
                 exit(2)
     except OSError:
-        print "OS error. Is 7z in PATH?"
+        print("OS error. Is 7z in PATH?")
         exit(4)
 
     if failed:
