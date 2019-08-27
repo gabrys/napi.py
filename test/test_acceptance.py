021dfdaf507627fd4d3c1459afd7fd8997fdb639
emkor93 <emkor93@gmail.com>
2019-08-27 19:47:00 +0200

    fix tests according to download_subs() API change

--- test/test_acceptance.py
+++ test/test_acceptance.py
@@ -7,7 +7,7 @@ class NapiPyAcceptanceTest(unittest.TestCase):
     def test_should_download_correctly_encoded_subs(self):
         movie_hash = '25b1087d997bfbf8a4be462255e05a05'
         napi = NapiPy()
-        src_enc, tmp_file = napi.download_subs(movie_hash)
+        src_enc, tgt_enc, tmp_file = napi.download_subs(movie_hash)
         self.assertEqual(src_enc, "utf-8")
 
         with open(tmp_file) as subs_file:
@@ -20,6 +20,6 @@ class NapiPyAcceptanceTest(unittest.TestCase):
     def test_should_return_none_when_no_subtitles_for_movie(self):
         movie_hash = '00000000000000000000000000000000'
         napi = NapiPy()
-        src_enc, tmp_file = napi.download_subs(movie_hash)
+        src_enc, tgt_enc, tmp_file = napi.download_subs(movie_hash)
         self.assertIsNone(src_enc)
         self.assertIsNone(tmp_file)
