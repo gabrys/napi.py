c35d33b15298583d032131d699d5252228d83850
emkor93 <emkor93@gmail.com>
2020-11-24 11:07:06 +0100

    rewrite the tests to use pytest approach

--- test/acceptance/test_acceptance_lib.py
+++ test/acceptance/test_acceptance_lib.py
@@ -1,38 +1,37 @@
-import unittest
-
 from napi import NapiPy
 
 
-class NapiPyAcceptanceTest(unittest.TestCase):
-    def test_should_download_correctly_encoded_subs(self):
-        movie_hash = '25b1087d997bfbf8a4be462255e05a05'
-        napi = NapiPy()
-        src_enc, tgt_enc, tmp_file = napi.download_subs(movie_hash)
-        self.assertEqual(src_enc, "utf-8")
+def test_should_download_correctly_encoded_subs():
+    movie_hash = '25b1087d997bfbf8a4be462255e05a05'
+    napi = NapiPy()
+    src_enc, tgt_enc, tmp_file = napi.download_subs(movie_hash)
+    assert src_enc == "utf-8"
 
-        with open(tmp_file) as subs_file:
-            subs = subs_file.read()
+    with open(tmp_file) as subs_file:
+        subs = subs_file.read()
 
-        expected_phrases = ['źródło', 'władzę', 'Dziękuję', 'zgłębiają', 'ZWYCIĘŻĄ!']
-        for expected_phrase in expected_phrases:
-            self.assertIn(expected_phrase, subs)
+    expected_phrases = ['źródło', 'władzę', 'Dziękuję', 'zgłębiają', 'ZWYCIĘŻĄ!']
+    for expected_phrase in expected_phrases:
+        assert expected_phrase in subs
 
-    def test_should_return_none_when_no_subtitles_for_movie(self):
-        movie_hash = '00000000000000000000000000000000'
-        napi = NapiPy()
-        src_enc, tgt_enc, tmp_file = napi.download_subs(movie_hash)
-        self.assertIsNone(src_enc)
-        self.assertIsNone(tmp_file)
 
-    def test_should_download_subs_with_forced_encoding(self):
-        movie_hash = '0e9b0d0d3dc5abc0538d207d477af4a1'
-        napi = NapiPy()
-        src_enc, tgt_enc, tmp_file = napi.download_subs(movie_hash, use_enc="utf-8")
-        self.assertEqual(src_enc, "utf-8")
+def test_should_return_none_when_no_subtitles_for_movie():
+    movie_hash = '00000000000000000000000000000000'
+    napi = NapiPy()
+    src_enc, tgt_enc, tmp_file = napi.download_subs(movie_hash)
+    assert src_enc is None
+    assert tmp_file is None
 
-        with open(tmp_file) as subs_file:
-            subs = subs_file.read()
 
-        expected_phrases = ['ciąży', 'artykułów', 'Właśnie', 'wyjść', 'wciąż']
-        for expected_phrase in expected_phrases:
-            self.assertIn(expected_phrase, subs)
+def test_should_download_subs_with_forced_encoding():
+    movie_hash = '0e9b0d0d3dc5abc0538d207d477af4a1'
+    napi = NapiPy()
+    src_enc, tgt_enc, tmp_file = napi.download_subs(movie_hash, use_enc="utf-8")
+    assert  src_enc == "utf-8"
+
+    with open(tmp_file) as subs_file:
+        subs = subs_file.read()
+
+    expected_phrases = ['ciąży', 'artykułów', 'Właśnie', 'wyjść', 'wciąż']
+    for expected_phrase in expected_phrases:
+        assert expected_phrase in subs
--- test/unit/test_sub_dl_and_conversion.py
+++ test/unit/test_sub_dl_and_conversion.py
@@ -1,5 +1,4 @@
 import os
-import unittest
 from os import path
 
 from napi.api import _build_url
@@ -15,28 +14,29 @@ def _get_project_dir(file_: str) -> str:
     return path.join(code_directory, proj_dir)
 
 
-class NapiPyTest(unittest.TestCase):
-    def test_should_generate_correct_url(self):
-        movie_hash = '6e7d92a7c0f40706067248d50d3b1d5a'
-        expected_url = 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=6e7d92a7c0f40706067248d50d3b1d5a&t=c6705&v=other&kolejka=false&nick=&pass=&napios={}'.format(
-            os.name)
-        actual_url = _build_url(movie_hash)
-        self.assertEqual(expected_url, actual_url)
+def test_should_generate_correct_url():
+    movie_hash = '6e7d92a7c0f40706067248d50d3b1d5a'
+    expected_url = 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=6e7d92a7c0f40706067248d50d3b1d5a&t=c6705&v=other&kolejka=false&nick=&pass=&napios={}'.format(
+        os.name)
+    actual_url = _build_url(movie_hash)
+    assert expected_url == actual_url
 
-    def test_should_unpack_downloaded_7zip(self):
-        subs_path = path.join(_get_project_dir(__file__), TEST_SUBS_7Z)
-        with open(subs_path, 'rb') as input_file:
-            compressed_content = input_file.read()
-        sub_bin_content = un7zip_api_response(compressed_content)
-        expected_phrase = b"GrupaHatak.pl"
-        self.assertIn(expected_phrase, sub_bin_content)
 
-    def test_should_encode_subtitles_correctly(self):
-        subs_path = path.join(_get_project_dir(__file__), TEST_SUBS_7Z)
-        with open(subs_path, 'rb') as input_file:
-            compressed_content = input_file.read()
-        sub_bin_content = un7zip_api_response(compressed_content)
-        _, sub_utf8_content = decode_subs(sub_bin_content)
-        expected_phrases = ['Tłumaczenie', 'Dunkierką', 'Francuzów', 'Uwięzieni']
-        for expected_phrase in expected_phrases:
-            self.assertIn(expected_phrase, sub_utf8_content)
+def test_should_unpack_downloaded_7zip():
+    subs_path = path.join(_get_project_dir(__file__), TEST_SUBS_7Z)
+    with open(subs_path, 'rb') as input_file:
+        compressed_content = input_file.read()
+    sub_bin_content = un7zip_api_response(compressed_content)
+    expected_phrase = b"GrupaHatak.pl"
+    assert expected_phrase in sub_bin_content
+
+
+def test_should_encode_subtitles_correctly():
+    subs_path = path.join(_get_project_dir(__file__), TEST_SUBS_7Z)
+    with open(subs_path, 'rb') as input_file:
+        compressed_content = input_file.read()
+    sub_bin_content = un7zip_api_response(compressed_content)
+    _, sub_utf8_content = decode_subs(sub_bin_content)
+    expected_phrases = ['Tłumaczenie', 'Dunkierką', 'Francuzów', 'Uwięzieni']
+    for expected_phrase in expected_phrases:
+        assert expected_phrase in sub_utf8_content
